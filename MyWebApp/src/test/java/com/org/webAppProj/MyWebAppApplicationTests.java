72ea814a5e5b34d963aba20ef731e251e11e5c62
RATNALA <pradeepkumar.ratnala@dxc.com>
2020-07-01 13:03:36 +0530

    first'

---
+++ MyWebApp/src/test/java/com/org/webAppProj/MyWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.webAppProj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
